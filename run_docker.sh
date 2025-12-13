af56dca818a68de69b687f20edbe5d42c65b2e96
Nikhil Sumesh <82290682+nsumesh@users.noreply.github.com>
2025-12-12 22:31:58 -0500

    commit docker

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Set default build arg
 BUILD_GAZEBO=${BUILD_GAZEBO_FROM_SOURCE:-false}
 
 # Allow X11 forwarding
@@ -17,16 +16,13 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     echo "X11 forwarding enabled for Mac (XQuartz)"
 fi
 
-# Build and run with docker-compose
 echo "Building Docker image (BUILD_GAZEBO_FROM_SOURCE=$BUILD_GAZEBO)..."
 BUILD_GAZEBO_FROM_SOURCE=$BUILD_GAZEBO docker-compose up --build -d
 
-# Enter container
 echo "Entering container..."
 echo "Note: All ROS 2 workspaces are already sourced in the container"
 docker-compose exec ros-gazebo bash
 
-# Cleanup on exit
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     xhost -local:docker 2>/dev/null
 elif [[ "$OSTYPE" == "darwin"* ]]; then
